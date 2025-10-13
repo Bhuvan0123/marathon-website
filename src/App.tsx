91d2759b64de1036e56650adce4e4f1658b292ee
Bhuvan0123 <bhuvanchandrap9@gmail.com>
2025-10-13 10:35:46 +0530

    netlify build done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Register from "./pages/Register";
+
 import Home from "./Home";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+   <Home/>
   );
 }
 
